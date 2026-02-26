ad134391973066f22d8c177d15080a155d5dedcc
Mr_Model <145706616+MrModel228@users.noreply.github.com>
2026-02-26 17:53:54 +0300

    Update FiguraFixClient.java

--- src/main/java/com/mrmodel/figurafix/FiguraFixClient.java
+++ src/main/java/com/mrmodel/figurafix/FiguraFixClient.java
@@ -1,1 +1,15 @@
+package com.mrmodel.figurafix;
 
+import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FiguraFixClient implements ClientModInitializer {
+    public static final String MOD_ID = "figurafix";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitializeClient() {
+        LOGGER.info("Figura Fix: Reign Edition успешно активирован, Сэр!");
+    }
+}
